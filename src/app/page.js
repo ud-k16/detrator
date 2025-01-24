902ecd6fc3b2ebc16f54c4eb9ba81d80aca73af2
uma <uma@tigeen.ch>
2025-01-24 15:06:04 +0530

    divider added

--- src/app/page.js
+++ src/app/page.js
@@ -1,63 +1,71 @@
-import { Avatar, Grid2, InputAdornment, TextField } from "@mui/material";
+import {
+  Avatar,
+  Divider,
+  Grid2,
+  InputAdornment,
+  TextField,
+} from "@mui/material";
 import { CiSearch } from "react-icons/ci";
 
 export default function Home() {
   return (
-    <Grid2
-      container
-      alignItems={"center"}
-      display={"flex"}
-      flexDirection={"row"}
-      boxSizing={"border-box"}
-      size={{ lg: 12 }}
-      paddingLeft={5}
-      paddingRight={5}
-      paddingTop={2}
-      sx={{
-        justifyContent: "space-evenly",
-      }}
-    >
-      <Grid2 size={{ lg: 2, xs: 10, sm: 10 }}>
-        <div>BrandName</div>
-      </Grid2>
+    <Grid2>
       <Grid2
-        size={{ lg: 8 }}
+        container
+        marginBottom={2}
+        alignItems={"center"}
+        display={"flex"}
+        flexDirection={"row"}
+        boxSizing={"border-box"}
+        size={{ lg: 12 }}
+        paddingLeft={5}
+        paddingRight={5}
+        paddingTop={2}
         sx={{
-          display: { xs: "none", lg: "block" },
+          justifyContent: "space-evenly",
         }}
       >
-        <TextField
-          size={"small"}
-          style={{ width: 400 }}
-          placeholder="what are you looking for?"
-          InputProps={{
-            endAdornment: (
-              <InputAdornment position="end">
-                <CiSearch />
-              </InputAdornment>
-            ),
-          }}
-        />
-      </Grid2>
-
-      <Grid2
-        size={{ lg: 2 }}
-        flexDirection={"row"}
-        display={"flex"}
-        alignItems={"center"}
-        columnGap={5}
-      >
-        <Avatar sx={{ xs: 2, sm: 2 }} />
+        <Grid2 size={{ lg: 2, xs: 10, sm: 10 }}>
+          <div>BrandName</div>
+        </Grid2>
         <Grid2
+          size={{ lg: 8 }}
           sx={{
             display: { xs: "none", lg: "block" },
           }}
         >
-          John Abraham pa
+          <TextField
+            size={"small"}
+            style={{ width: 400 }}
+            placeholder="what are you looking for?"
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                  <CiSearch />
+                </InputAdornment>
+              ),
+            }}
+          />
+        </Grid2>
+
+        <Grid2
+          size={{ lg: 2 }}
+          flexDirection={"row"}
+          display={"flex"}
+          alignItems={"center"}
+          columnGap={5}
+        >
+          <Avatar sx={{ xs: 2, sm: 2 }} />
+          <Grid2
+            sx={{
+              display: { xs: "none", lg: "block" },
+            }}
+          >
+            John Abraham pa
+          </Grid2>
         </Grid2>
       </Grid2>
-
-      <Grid2 size="grow"></Grid2>
+      <Divider />
     </Grid2>
   );
 }

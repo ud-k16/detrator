69f68b311f57197db54b825e059818c1888d2cd3
uma <uma@tigeen.ch>
2025-01-24 12:23:33 +0530

    basic structure addded

--- src/app/layout.js
+++ src/app/layout.js
@@ -19,6 +19,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </head>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
       </body>
--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,63 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import { Avatar, Grid2, InputAdornment, TextField } from "@mui/material";
+import { CiSearch } from "react-icons/ci";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
+    <Grid2
+      container
+      alignItems={"center"}
+      display={"flex"}
+      flexDirection={"row"}
+      boxSizing={"border-box"}
+      size={{ lg: 12 }}
+      paddingLeft={5}
+      paddingRight={5}
+      paddingTop={2}
+      sx={{
+        justifyContent: "space-evenly",
+      }}
+    >
+      <Grid2 size={{ lg: 2, xs: 10, sm: 10 }}>
+        <div>BrandName</div>
+      </Grid2>
+      <Grid2
+        size={{ lg: 8 }}
+        sx={{
+          display: { xs: "none", lg: "block" },
+        }}
+      >
+        <TextField
+          size={"small"}
+          style={{ width: 400 }}
+          placeholder="what are you looking for?"
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <CiSearch />
+              </InputAdornment>
+            ),
+          }}
         />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+      </Grid2>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+      <Grid2
+        size={{ lg: 2 }}
+        flexDirection={"row"}
+        display={"flex"}
+        alignItems={"center"}
+        columnGap={5}
+      >
+        <Avatar sx={{ xs: 2, sm: 2 }} />
+        <Grid2
+          sx={{
+            display: { xs: "none", lg: "block" },
+          }}
         >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+          John Abraham pa
+        </Grid2>
+      </Grid2>
+
+      <Grid2 size="grow"></Grid2>
+    </Grid2>
   );
 }
